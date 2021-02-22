a240bfcf10f004b1ad5da6c182bd2d7b0fd68b0a
diana <diana@DESKTOP-AE9RS0A>
2021-02-22 15:48:23 +0300

    Добавьте файлы проекта.

---
+++ Complex_number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Complex_number
+    {
+        public double a1 { get; set; } //Действительная часть числа
+        public double b1 { get; set; } //Мнимая часть числа
+
+        //Конструктор
+        public Complex_number(double a1, double b1)
+        {
+            this.a1 = a1;
+            this.b1 = b1;
+        }
+
+        //Функция вычисления модуля
+        public double ABC()
+        {
+            return Math.Sqrt(a1 * a1 + b1 * b1);
+        }
+
+        //Функция получения обратного числа
+        public double getReverseNumberA()
+        {
+            double d = ((a1 * a1) + (b1 * b1));
+            return a1 / d;
+        }
+        public double getReverseNumberB()
+        {
+            double d = ((a1 * a1) + (b1 * b1));
+            return -b1 / d;
+        }
+    }
+}
+
---
+++ Complex_number2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Complex_number2 : Complex_number
+    {
+        public double a2 { get; set; }
+        public double b2 { get; set; }
+
+        public Complex_number2(double a2, double b2)
+        {
+            this.a2 = a2;
+            this.b2 = b2;
+        }
+        public double multiplication(double a1, double b1, double a2, double b2) 
+        {
+            return (a1*a2-b1*b2) + (a1 * b2 + a2 * b1);
+        }
+    }
+}
