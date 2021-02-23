93b5dfc59231fb483352d77bc145957d4eebec5c
diana <diana@DESKTOP-AE9RS0A>
2021-02-23 16:36:18 +0300

    1

--- Complex_number2.cs
+++ Complex_number2.cs
@@ -8,17 +8,22 @@ namespace Inheritance
 {
     class Complex_number2 : Complex_number
     {
-        public double a2 { get; set; }
-        public double b2 { get; set; }
-
-        public Complex_number2(double a2, double b2)
+        public Complex_number2(double a1, double b1, double a2, double b2) : base(a1, b1)
         {
             this.a2 = a2;
             this.b2 = b2;
         }
+
+        public double a2 { get; set; } //Действительна часть числа
+        public double b2 { get; set; } //Мнимая часть числа
+
+        //Конструктор
+        
+
+        //Функция умножения двух комплексных чисел
         public double multiplication(double a1, double b1, double a2, double b2) 
         {
-            return (a1*a2-b1*b2) + (a1 * b2 + a2 * b1);
+            return (a1*b2-b1*a2) + ((a1*a2-b1*b2) + (a1 * b2 + a2 * b1));
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace Inheritance
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Получение данных от пользователя
+            //
+            double a1 = double.Parse(A1.Text);
+            double b1 = double.Parse(B1.Text);
+            //
+            double a2 = double.Parse(A2.Text);
+            double b2 = double.Parse(B2.Text);
+
+            //Создание объектa класса
+            Complex_number2 num2 = new Complex_number2(a1, b1, a2, b2);
+            //Создание результирующей переменной и вызов метода
+            double result = num2.multiplication(a1, b1, a2, b2);
+
+            //Вывод результата
+            OUTPUT.Text += "Произведение двух комплексных чисел равно: " + result + Environment.NewLine;
+        }
     }
 }
